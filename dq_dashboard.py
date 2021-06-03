e49a55cd0125141948fd493702b58fc69278dd13
Katre <Akshay.Katre@rabobank.nl>
2021-06-03 20:12:07 +0200

    dq dashboard added

--- dq_dashboard.py
+++ dq_dashboard.py
@@ -1,40 +1,32 @@
-# -*- coding: utf-8 -*-
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.express as px
+import dash_table
 import pandas as pd
 
+df = pd.read_csv('test_data.csv', index_col=False)
+df = df.fillna(0)
+
+app = dash.Dash(__name__)
+
+app.layout = dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in df.columns],
+    data=df.to_dict('records'),
+    filter_action="native",
+    sort_action="native",
+    sort_mode="multi",
+    page_size=25,
+    style_data_conditional=[
+        {
+            'if':{
+                'filter_query': '{Per_Missing_val} > 0.01',
+            },
+            'backgroundColor': '#FF4136',
+            'color': 'white'         
+        }
+    ]
+)
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
-])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
